03085b33d61531b6166610378ee2531c44c5d8e5
jayantkir-create <jayantkir@gmail.com>
2025-11-23 01:39:13 +0530

    Add files via upload

---
+++ incometax.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class incometax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int income = sc.nextInt();
+        int tax;
+        if (income <= 500000){
+            tax = 0;
+            System.out.println("No tax");
+        } else if (income > 500000 && income <= 1000000){
+            tax = (int)(income * 0.2);
+            System.out.println("Tax is: " + tax);
+        } else {
+            tax = (int)(income * 0.3);  
+            System.out.println("Tax is: " + tax);
+        }
+        sc.close();
+    }
+}
